e536d8132a90eff7f29e4b8f59b6f444b59d9967
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-05 15:50:27 -0500

    Configure client routes so Heroku deployment functions properly

--- server/server.js
+++ server/server.js
@@ -57,6 +57,19 @@ app.post('/ticket', (req, res) => tickets.postTicketData(req, res, db))
 app.put('/ticket', (req, res) => tickets.putTicketData(req, res, db))
 app.delete('/ticket', (req, res) => tickets.deleteTicketData(req, res, db))
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // App Server Connection
 app.listen(process.env.PORT || 5000, () => {
   console.log(`app is running on port ${process.env.PORT || 5000}`)
