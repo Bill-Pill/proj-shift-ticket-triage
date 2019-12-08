519bfc63d82404664b86c2bded9596a9b4063f50
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-08 14:00:26 -0500

    Add TicketTable component rendering live data from DB

--- server/client/src/components/TicketTable.js
+++ server/client/src/components/TicketTable.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { fetchTickets } from "../actions"
 import { Table, Divider, Tag } from 'antd';
 
-const { Column, ColumnGroup } = Table;
+const { Column } = Table;
 
 const data = [
   {
@@ -34,45 +35,61 @@ const data = [
 
 class TicketTable extends Component {
 
-  
+  componentDidMount() {
+    this.props.fetchTickets()
+  }
+
 
   render() {
     return (
-      <Table dataSource={data}>
-        <ColumnGroup title="Name">
-          <Column title="First Name" dataIndex="firstName" key="firstName" />
-          <Column title="Last Name" dataIndex="lastName" key="lastName" />
-        </ColumnGroup>
-        <Column title="Age" dataIndex="age" key="age" />
-        <Column title="Address" dataIndex="address" key="address" />
-        <Column
-          title="Tags"
-          dataIndex="tags"
-          key="tags"
-          render={tags => (
-            <span>
-              {tags.map(tag => (
-                <Tag color="blue" key={tag}>
-                  {tag}
-                </Tag>
-              ))}
-            </span>
-          )}
-        />
-        <Column
-          title="Action"
-          key="action"
-          render={(text, record) => (
-            <span>
-              <a>Invite {record.lastName}</a>
-              <Divider type="vertical" />
-              <a>Delete</a>
-            </span>
-          )}
-        />
+      <Table dataSource={this.props.tickets}>
+        <Column title="Ticket ID" dataIndex="ticketid" key="ticketid" />
+        <Column title="Status" dataIndex="statuscode" key="statuscode" />
+        <Column title="Title" dataIndex="title" key="title" />
+        <Column title="Category" dataIndex="category" key="category" />
+        <Column title="Email" dataIndex="email" key="email" />
+        <Column title="Department" dataIndex="department" key="department" />
+        <Column title="Date Submitted" dataIndex="added" key="added" />
       </Table>
+      // <Table dataSource={data}>
+      //     <Column title="First Name" dataIndex="firstName" key="firstName" />
+      //     <Column title="Last Name" dataIndex="lastName" key="lastName" />
+      //   <Column title="Age" dataIndex="age" key="age" />
+      //   <Column title="Address" dataIndex="address" key="address" />
+      //   <Column
+      //     title="Tags"
+      //     dataIndex="tags"
+      //     key="tags"
+      //     render={tags => (
+      //       <span>
+      //         {tags.map(tag => (
+      //           <Tag color="blue" key={tag}>
+      //             {tag}
+      //           </Tag>
+      //         ))}
+      //       </span>
+      //     )}
+      //   />
+      //   <Column
+      //     title="Action"
+      //     key="action"
+      //     render={(text, record) => (
+      //       <span>
+      //         <a>Invite {record.lastName}</a>
+      //         <Divider type="vertical" />
+      //         <a>Delete</a>
+      //       </span>
+      //     )}
+      //   />
+      // </Table>
     )
   }
 }
 
-export default connect(null)(TicketTable);
+const mapStateToProps = state => {
+  return {
+   tickets: state.tickets
+  };
+};
+
+export default connect(mapStateToProps, { fetchTickets })(TicketTable);
