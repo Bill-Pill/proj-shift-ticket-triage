b5e63762db719cb2946bf9a147d98fb228cffbcc
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-08 15:06:10 -0500

    Ticket titles Link to ticket details(currently empty)

--- server/client/src/components/TicketTable.js
+++ server/client/src/components/TicketTable.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom'
 import { fetchTickets } from "../actions"
 import { Table} from 'antd';
 
-const { Column } = Table;
+// Schema used for Ticket Table component w/ Ant Design
+const columns = [
+  {
+    title: 'Ticket ID',
+    dataIndex: 'ticketid',
+    key: 'ticketid'
+  },
+  {
+    title: 'Status',
+    dataIndex: 'statuscode',
+    key: 'statuscode'
+  },
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text, record) => <Link to={'ticket/' + record.ticketid}>{text}</Link>
+  },
+  {
+    title: 'Category',
+    dataIndex: 'category',
+    key: 'category'
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email'
+  },
+  {
+    title: 'Department',
+    dataIndex: 'department',
+    key: 'department'
+  },
+  {
+    title: 'Ticket Submit Date',
+    dataIndex: 'date',
+    key: 'date'
+  },
+]
 
 class TicketTable extends Component {
 
@@ -17,48 +56,9 @@ class TicketTable extends Component {
 
   render() {
     return (
-      <Table dataSource={this.props.tickets} rowKey="ticketid" onRow={r => ({
-        onClick: () => this.HandleRowClick(r.ticketid)
-      })}>
-        <Column title="Ticket ID" dataIndex="ticketid" key="ticketid" />
-        <Column title="Status" dataIndex="statuscode" key="statuscode" />
-        <Column title="Title" dataIndex="title" key="title" />
-        <Column title="Category" dataIndex="category" key="category" />
-        <Column title="Email" dataIndex="email" key="email" />
-        <Column title="Department" dataIndex="department" key="department" />
-        <Column title="Date Submitted" dataIndex="added" key="added" />
+      <Table dataSource={this.props.tickets} columns={columns}
+        rowKey="ticketid">
       </Table>
-      // <Table dataSource={data}>
-      //     <Column title="First Name" dataIndex="firstName" key="firstName" />
-      //     <Column title="Last Name" dataIndex="lastName" key="lastName" />
-      //   <Column title="Age" dataIndex="age" key="age" />
-      //   <Column title="Address" dataIndex="address" key="address" />
-      //   <Column
-      //     title="Tags"
-      //     dataIndex="tags"
-      //     key="tags"
-      //     render={tags => (
-      //       <span>
-      //         {tags.map(tag => (
-      //           <Tag color="blue" key={tag}>
-      //             {tag}
-      //           </Tag>
-      //         ))}
-      //       </span>
-      //     )}
-      //   />
-      //   <Column
-      //     title="Action"
-      //     key="action"
-      //     render={(text, record) => (
-      //       <span>
-      //         <a>Invite {record.lastName}</a>
-      //         <Divider type="vertical" />
-      //         <a>Delete</a>
-      //       </span>
-      //     )}
-      //   />
-      // </Table>
     )
   }
 }
