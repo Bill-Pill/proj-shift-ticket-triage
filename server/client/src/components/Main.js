bb1a4ec634bb14bbebf26c5badbd67f1cee0caee
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-08 10:16:47 -0500

    Move column/row logic to Main component

--- server/client/src/components/Main.js
+++ server/client/src/components/Main.js
@@ -5,6 +5,8 @@ import { Row, Col, Layout, Breadcrumb } from 'antd'
 import TicketForm from './TicketForm'
 import TicketProgress from './TicketProgress'
 
+import { Timeline } from 'antd';
+
 const { Header, Content, Footer, Sider } = Layout;
 
 class Main extends Component {
@@ -41,15 +43,29 @@ class Main extends Component {
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
+            
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
               Hi I'm the main component. Ticket data below:
               {this.renderTickets()}
-              <TicketProgress />
+              <Timeline>
+                <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+                <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+                <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+              </Timeline>
+              <div className="container-status">
+                <Row type="flex" align="middle" className="row-status">
+                  <Col span={20} offset={2}>
+                    <TicketProgress />
+                  </Col>
+                  <Col span={2}></Col>
+                </Row>
+              </div>
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>Ticket Triage!</Footer>
         </Layout>
-        <Sider width={500} reverseArrow
+        <Sider width={350} reverseArrow
           collapsible collapsed={this.state.collapsed} 
           onCollapse={this.onCollapse}>
           <div>
--- server/client/src/components/TicketProgress.js
+++ server/client/src/components/TicketProgress.js
@@ -5,26 +5,21 @@ import { Steps, Row, Col } from 'antd'
 const { Step } = Steps;
 
 class TicketProgress extends Component {
+  state = {
+    currentStep: 0
+  };
 
   render() {
     return (
-        <div className="container-status">
-          <Row type="flex" align="middle" className="row-status">
-            <Col span={20} offset={2}>
-              <h3>Step progress bar changes to vertical on mobile. Animations??</h3>
-              <Steps current={2} status="error">
-                <Step title="Ticket Sent" description="Your ticket is sent and waiting for review" />
-                <Step title="Resolving Issue" description="(USERNAME) is now hard at 
-                  work resolving the issue. --------------------
-                  ----------------- **COLORCHANGE** (USERNAME) is
-                  typing up a response"/>
-                <Step title="Response Requested" description="(USERNAME) has requested a response" />
-                <Step title="Issue Resolved" description="Huzzah! Ticket is now resolved and closed" />
-              </Steps>
-            </Col>
-            <Col span={2}></Col>
-          </Row>
-        </div>
+      <Steps current={this.state.currentStep} status="process">
+        <Step title="Ticket Sent" description="Your ticket is sent and waiting for review" />
+        <Step title="Resolving Issue" description="(USERNAME) is now hard at 
+          work resolving the issue. --------------------
+          ----------------- **COLORCHANGE** (USERNAME) is
+          typing up a response"/>
+        <Step title="Response Requested" description="(USERNAME) has requested a response" />
+        <Step title="Issue Resolved" description="Huzzah! Ticket is now resolved and closed" />
+      </Steps>
     );
   }
 }
