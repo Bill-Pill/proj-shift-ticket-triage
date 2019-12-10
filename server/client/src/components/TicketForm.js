db2246932e4e0bf2c3b678fbaba93f202dd9eac4
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-10 09:30:30 -0500

    TicketForm values now correctly update local state

--- server/client/src/components/TicketForm.js
+++ server/client/src/components/TicketForm.js
@@ -10,18 +10,28 @@ class TicketForm extends Component {
 
     this.state = {
       title: '',
-      category: '',
+      category: 'Bug',
       department: 'Testing',
       ticketdetails: ''
     };
   }
 
   onChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name
+      this.setState ({ [name]:value })
+  }
+
+  onCategoryChange = (e) => {
     const target = e.target;
-    console.log(target)
+    if (target.checked) {
+      const value = target.value
+      this.setState({ category: value})
+    }
   }
 
   render() {
+    console.log('default state: ', this.state)
     return (
       <Form className='ticket-form' onSubmit={this.handleSubmit}>
         <h3 style={{ color: 'yellowgreen' }}>Create a Ticket</h3>
@@ -36,11 +46,11 @@ class TicketForm extends Component {
           label="Category">
           <div className="icons-list categories">
             <Radio.Group buttonStyle="solid" defaultValue="Bug">
-              <Radio.Button value="Bug"><Icon type="bug"/>Bug</Radio.Button>
-              <Radio.Button value="Network"><Icon type="api"/>Internet/Network</Radio.Button>
-              <Radio.Button value="Computer"><Icon type="laptop"/>Computer</Radio.Button>
-              <Radio.Button value="Phone"><Icon type="phone"/>Phone</Radio.Button>
-              <Radio.Button value="Software"><Icon type="dashboard"/>Software</Radio.Button>
+              <Radio.Button value="Bug" onChange={this.onCategoryChange}><Icon type="bug"/>Bug</Radio.Button>
+              <Radio.Button value="Network" onChange={this.onCategoryChange}><Icon type="api"/>Internet/Network</Radio.Button>
+              <Radio.Button value="Computer" onChange={this.onCategoryChange}><Icon type="laptop"/>Computer</Radio.Button>
+              <Radio.Button value="Phone" onChange={this.onCategoryChange}><Icon type="phone"/>Phone</Radio.Button>
+              <Radio.Button value="Software" onChange={this.onCategoryChange}><Icon type="dashboard"/>Software</Radio.Button>
             </Radio.Group>
           </div>
         </Form.Item>
@@ -49,7 +59,7 @@ class TicketForm extends Component {
           <TextArea 
             placeholder="My computer is unplugged but I'm not getting any power for some reason."
             rows={10}
-            name='details'
+            name='ticketdetails'
             onChange={ this.onChange } />
         </Form.Item>
         <Form.Item>
