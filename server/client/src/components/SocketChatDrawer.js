7cbfeef14a00152b476059a49f0fbe9aca38ad49
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-10 16:25:22 -0500

    Socket drawer can send messages to the server

--- server/client/src/components/SocketChatDrawer.js
+++ server/client/src/components/SocketChatDrawer.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Drawer, Button, Radio } from 'antd';
-
-const RadioGroup = Radio.Group;
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:3000')
 
 class SocketChatDrawer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ''
+    };
+  }
+
+  onChange = (e) => {
+    let value = e.target.value
+    this.setState({message: value})
+    console.log(value)
+  }
+
+  keyPressed = (e) => {
+    if (e.key === 'Enter') {
+      this.sendMessage()
+    }
+  }
+
+    sendMessage = () => {
+      const message = this.state.message
+      // if there is a non-empty message and a socket connection
+      if (message) {
+
+        // shows visible message
+        // addChatMessage({
+        //   username: 'socketTester',
+        //   message: message
+        // });
+
+        // tell server to execute 'new message' and send along one parameter
+        socket.emit('new message', message);
+      }
+   }
 
   render() {
     return (
@@ -16,9 +51,16 @@ class SocketChatDrawer extends Component {
           onClose={this.onClose}
           visible
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          <ul className="pages">
+          <li className="chat page">
+            <div className="chatArea">
+              <ul className="messages"></ul>
+            </div>
+            <input onKeyPress={this.keyPressed} 
+            onChange={this.onChange}
+            className="inputMessage" placeholder="Type here..."/>
+          </li>
+        </ul>
         </Drawer>
       </div>
     );
--- server/server.js
+++ server/server.js
@@ -80,7 +80,65 @@ if (process.env.NODE_ENV === 'production') {
 // Sockets & Express hookup
 const server = http.createServer(app)
 const io = module.exports.io = require('socket.io').listen(server)
-io.on('connection', SocketManager)
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+
+    console.log('new message: ', data)
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
 
 // App Server Connection
 server.listen(process.env.PORT || 5000, () => {
