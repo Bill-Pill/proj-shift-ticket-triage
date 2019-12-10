ba8956bc04fe85b5620110ce1d2b34a7e98da4b8
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-12-10 11:24:11 -0500

    troubleshooting my axios requests not using proxy in clientside package.json

--- server/server.js
+++ server/server.js
@@ -26,17 +26,6 @@ const tickets = require('./controllers/tickets')
 // App
 const app = express()
 
-// App Middleware
-// const whitelist = ['http://localhost:3000']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
 app.use(helmet())
 app.use(cors())
 // app.use(bodyParser.json())
